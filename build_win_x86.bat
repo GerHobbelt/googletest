4bd841c317eab09b70c55503cfdf4e8514b32d8f
lo3ck <mingtaoguan@infivision.com.cn>
2021-01-24 23:30:37 +0800

    add windows compile script

--- build_win_x86.bat
+++ build_win_x86.bat
@@ -6,7 +6,9 @@ cmake -A Win32  -DCMAKE_CXX_COMPILER="c++" ../
 set devenv_dir="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv"
 echo "start build Release"
 %devenv_dir% googletest-distribution.sln /Build "Release|Win32"
+echo "start build Debug"
+%devenv_dir% googletest-distribution.sln /Build "Debug|Win32"
 
-
-md ../infistd/lib/win/x86/
-rem cp -v build/lib/libgtest.a ../infistd/lib/osx/x86/
+md ..\infistd\lib\win\x86\
+xcopy lib\Release\gtest.lib ..\..\infistd\lib\win\x86\Release\  /s /e /c /y /h /r
+xcopy lib\Debug\gtestd.lib ..\..\infistd\lib\win\x86\Debug\  /s /e /c /y /h /r
